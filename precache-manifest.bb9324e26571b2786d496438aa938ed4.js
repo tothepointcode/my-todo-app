08cb562e9c02c4adafafb87186de81b762638bcb
Terra Andoh Baffoe <terra.andoh@gmail.com>
2019-11-11 15:28:07 +0000

    Updates

---
+++ precache-manifest.bb9324e26571b2786d496438aa938ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5cba1bf4e68d3be4111d33d142abe9",
+    "url": "/my-todo-app/index.html"
+  },
+  {
+    "revision": "7537d59645a2183ce555",
+    "url": "/my-todo-app/static/css/main.982f0420.chunk.css"
+  },
+  {
+    "revision": "2122fafb2007a2586e95",
+    "url": "/my-todo-app/static/js/2.625f144a.chunk.js"
+  },
+  {
+    "revision": "7537d59645a2183ce555",
+    "url": "/my-todo-app/static/js/main.5ce6e413.chunk.js"
+  },
+  {
+    "revision": "6a202af9ad4d44d2ea2c",
+    "url": "/my-todo-app/static/js/runtime~main.d2b0c0c2.js"
+  }
+]);
